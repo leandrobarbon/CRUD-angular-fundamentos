370d33ceba24d881bc195aa01c5172f29a302477
Leandro lombaldo <leandrosilva.hair@gmail.com>
2023-05-28 21:09:24 -0300

    #estudando fundamentos Angular crud

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent {
+
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: 'modelo1'
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+   ) {}
+
+  //trecho de código omitido
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+
+  //trecho de código omitido
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
+  }
+}
